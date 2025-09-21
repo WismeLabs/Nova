784ac8908a46a549e8f5e07e8ed7b123cdc19e1a
NISomSOM <163893487+NISomSOM@users.noreply.github.com>
2025-09-21 19:38:19 +0530

    login and signup page initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
 }
 
@@ -49,7 +50,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
     // 🔧 Add your feature-specific dependencies here:
     // Example: Hilt for Dependency Injection
     // implementation("com.google.dagger:hilt-android:2.48")
