06e0e6f306e1c2dcd63fbed871add2a6b040a1bf
NISomSOM <163893487+NISomSOM@users.noreply.github.com>
2025-09-22 12:49:44 +0530

    login viewmodel added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -56,6 +57,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.auth.ktx)
 
     // 🔧 Add your feature-specific dependencies here:
     // Example: Hilt for Dependency Injection
